b191f485cb2c44b5009fe41a4b4cd3a51860f01d
volving <volving@icloud.com>
2019-10-17 11:42:48 +0800

    Add: snake_case support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # vno-camelcase
 
-A VSCode extension which turns selected texts into their camel-cased form.
+A VSCode extension which turns selected texts into their camelCased and snake_cased form.
 
 ## Features
 
@@ -11,7 +11,7 @@ A VSCode extension which turns selected texts into their camel-cased form.
 
 ## Requirements
 
-No~
+- vscode: ^1.36.0
 
 ## Extension Settings
 
@@ -27,6 +27,10 @@ For example:
     {
         "key": "cmd+h cmd+c",
         "command": "extension.ccase"
+    },
+    {
+        "key": "cmd+h cmd+s",
+        "command": "extension.scase"
     }
   ]
 ```
@@ -37,12 +41,19 @@ No till now.
 
 ## Release Notes
 
+### 1.1.0
+
+Support:
+
+1. transform text into snake_cased
+
 ### 1.0.0
 
 Support:
 
-1. single-line & multi-line selections
-2. word connectors can be '-, \_' and their arbitory compositions
+1. transform text into camelCased
+2. single-line & multi-line selections
+3. word connectors can be '-, \_' and their arbitory compositions
 
 -----------------------------------------------------------------------------------------------------------
 PRs are welcome!
--- extension.js
+++ extension.js
@@ -5,47 +5,101 @@ const editor = vscode.window.activeTextEditor;
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-  let disposable = vscode.commands.registerCommand(
-    'extension.ccase',
-    function() {
-      editor.selections.forEach(function(selection, idx) {
-        setTimeout(() => {
-          camelCaseSelected(selection);
-        }, 5*idx);
-      });
-    }
-  );
-  context.subscriptions.push(disposable);
+    // CMD: camelCase
+    let ccase = vscode.commands.registerCommand('extension.ccase', function() {
+        editor.selections.forEach(function(selection, idx) {
+            setTimeout(() => {
+                camelCaseSelected(selection);
+            }, 5 * idx);
+        });
+    });
+    context.subscriptions.push(ccase);
+
+    // CMD: snakeCase
+    let scase = vscode.commands.registerCommand('extension.scase', function() {
+        editor.selections.forEach(function(selection, idx) {
+            setTimeout(() => {
+                snakeCaseSelected(selection);
+            }, 5 * idx);
+        });
+    });
+    context.subscriptions.push(scase);
 }
 
 function deactivate() {}
 
-function camelCaseSelected(selection) {
-  let selected = editor.document.getText(selection).trim();
-
-  if (checkText(selected, vscode.window.showInformationMessage)) {
-    editor.edit(editBuilder => {
-      console.log('editing: ' + selected + ' ' + selection);
-      editBuilder.replace(selection, camelCaseText(selected));
-    });
-  }
-}
+/**
+ *
+ * @param {text} text
+ * @param {function} cb
+ */
 function checkText(text, cb) {
-  cb = cb && console.log;
-  if (!(text && text.length > 0)) {
-    cb('Error selection!');
-    return false;
-  }
-  return true;
+    cb = cb && console.log;
+    if (!(text && text.length > 0)) {
+        cb('Error selection!');
+        return false;
+    }
+    return true;
 }
 
 function camelCaseText(text) {
-  return text.replace(/([-_]+(\w))/g, (all, first, second) =>
-    second.toUpperCase()
-  );
+    return text.replace(/([-_]+(\w))/g, (all, first, second) =>
+        second.toUpperCase()
+    );
+}
+function snakeCaseText(text) {
+    return text.replace(/([A-Z])/g, (all, first) => `_${first.toLowerCase()}`);
+}
+/*
+function camelCaseSelected(selection) {
+    let selected = editor.document.getText(selection).trim();
+
+    if (checkText(selected, vscode.window.showInformationMessage)) {
+        editor.edit(editBuilder => {
+            console.log('editing: ' + selected + ' ' + selection);
+            editBuilder.replace(selection, camelCaseText(selected));
+        });
+    }
 }
 
+function snakeCaseSelected(selection) {
+    let selected = editor.document.getText(selection).trim();
+    function snakeCaseText(text) {
+        let ccasedText = camelCaseText(text)
+        return ccasedText.replace(/([A-Z])/g, (all, first) =>
+            `_${first.toLowerCase()}`
+        );
+    }
+    if (checkText(selected, vscode.window.showInformationMessage)) {
+        editor.edit(editBuilder => {
+            console.log('editing: ' + selected + ' ' + selection);
+            editBuilder.replace(selection, snakeCaseText(selected));
+        });
+    }
+}*/
+// {ccase: camelCase, scase: snake_case}
+
+function camelCaseSelected(selection) {
+    xCaseSelected(selection);
+}
+function snakeCaseSelected(selection) {
+    xCaseSelected(selection, 'scase');
+}
+
+function xCaseSelected(selection, mode = 'ccase') {
+    let selected = editor.document.getText(selection).trim();
+    let processedText = null;
+    if (checkText(selected, vscode.window.showInformationMessage)) {
+        editor.edit(editBuilder => {
+            processedText = camelCaseText(selected);
+            if (mode === 'scase') {
+                processedText = snakeCaseText(processedText);
+            }
+            editBuilder.replace(selection, processedText);
+        });
+    }
+}
 module.exports = {
-  activate,
-  deactivate,
+    activate,
+    deactivate
 };
